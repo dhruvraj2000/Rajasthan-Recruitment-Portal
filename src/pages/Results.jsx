a1a322d4d9e98e15b6c4d205d5d644279e1e3e14
Dhruv Raj Kumawat <63605080+dhruvraj2000@users.noreply.github.com>
2025-12-04 16:55:04 +0530

    Language Hindi to English part done

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,100 +1,179 @@
-import { Input, Form, message, Dropdown, Space, Breadcrumb, Row, Col } from "antd";
+import {
+  Input,
+  Form,
+  message,
+  Dropdown,
+  Space,
+  Breadcrumb,
+  Row,
+  Col,
+} from "antd";
 import CustomDatePicker from "../components/Calender";
 import { SoundOutlined, RedoOutlined } from "@ant-design/icons";
 import CustomButton from "../components/Button";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CustomInput from "../components/Input";
-import { DownOutlined } from "@ant-design/icons";
 import PagePartition from "../components/Breadcrumb";
 import CaptchaBox from "../components/CaptchaBox";
 import CustomDropdown from "../components/CustomDropdown";
 import Pageheader from "../components/PageHeader";
-// import HeadingBar from "../components/HeadingBar";
-const Result = () => {
+import Language from "../data/Language";
+
+import {
+  SkeletonInput,
+  SkeletonButton,
+  SkeletonLine,
+} from "../components/SkeletonComponent";
+
+const Result = ({ currentLang }) => {
   const [form] = Form.useForm();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, [currentLang]);
 
   const onFinish = (values) => {
-    message.success(`Fetching SSO ID for ${values.applicationNo}...`);
-    navigate(
-      `/viewssoidbyapplication?applicationNo=${
-        values.applicationNo
-      }&dob=${values.dob.format("YYYY-MM-DD")}`
-    );
+    message.success(`Fetching result for ${values.applicationNo}...`);
   };
-  const optons = [
+
+  const options = [
     { label: "Pre Exam", key: "Pre Exam" },
     { label: "Main Exam", key: "Main Exam" },
     { label: "Interview", key: "Interview" },
-  ]
+  ];
 
   return (
     <>
       <div>
+        {loading ? (
+          <SkeletonLine width="35%" />
+        ) : (
+          <Pageheader title={Language[currentLang].result} />
+        )}
 
-        <Pageheader title="Get Result" />
-      {/* <HeadingBar title="Get Result" /> */}
-      <PagePartition
-        items={[{ title: "Home", to: "/" }, { title: <span style={{fontWeight: 700}}> Get Result </span> }]}
-      />
-      <div className="form-format">
-          <Form form={ form } layout="vertical" className="form-style">
+        {loading ? (
+          <SkeletonLine width="50%" />
+        ) : (
+          <PagePartition
+            items={[
+              { title: Language[currentLang].home, to: "/" },
+              {
+                title: (
+                  <span style={{ fontWeight: 700 }}>
+                    {Language[currentLang].result}
+                  </span>
+                ),
+              },
+            ]}
+          />
+        )}
+
+        <div className="form-format">
+          <Form form={form} layout="vertical" className="form-style">
             <Row gutter={[8, 8]}>
               <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                  <Form.Item label="Select Exam Type" name="examType">
-            <CustomDropdown options={optons}/>
-
-          </Form.Item>
+                {loading ? (
+                  <>
+                    <SkeletonLine width="50%" />
+                    <SkeletonInput />
+                  </>
+                ) : (
+                  <Form.Item
+                    label={Language[currentLang].resultDropDown}
+                    name="examType"
+                  >
+                    <CustomDropdown
+                      options={options}
+                      currentLang={currentLang}
+                      defaultValue={null}
+                    />
+                  </Form.Item>
+                )}
               </Col>
-              <Col xs={ 24 } sm={ 24 } md={ 8 } lg={ 8 } xl={ 8 }>
 
- <Form.Item label="Application No" name="applicationNo">
-            <CustomInput placeholder="Enter Application Number" />
-          </Form.Item>
+              <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                {loading ? (
+                  <>
+                    <SkeletonLine width="60%" />
+                    <SkeletonInput />
+                  </>
+                ) : (
+                  <Form.Item
+                    label={Language[currentLang].ApplicationNo}
+                    name="applicationNo"
+                  >
+                    <CustomInput
+                      placeholder={Language[currentLang].applicationPlaceholder}
+                    />
+                  </Form.Item>
+                )}
               </Col>
-              <Col xs={ 24 } sm={ 24 } md={ 8 } lg={ 8 } xl={ 8 }>
-                  <Form.Item label="Date Of Birth" name="dob">
-            <CustomDatePicker placeholder="Enter Date of Birth" />
-          </Form.Item>
-               </Col>
 
-              <Col xs={ 24 } sm={ 24 } md={ 8 } lg={ 8 } xl={ 8 }>
-                   <Form.Item label="CAPTCHA" name="captcha">
+              <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                {loading ? (
+                  <>
+                    <SkeletonLine width="40%" />
+                    <SkeletonInput />
+                  </>
+                ) : (
+                  <Form.Item label={Language[currentLang].dob} name="dob">
+                    <CustomDatePicker
+                      placeholder={Language[currentLang].dobPlacerholder}
+                    />
+                  </Form.Item>
+                )}
+              </Col>
 
-            <CaptchaBox
-              length={6}
-              onVerify={(isValid) => {
-                if (isValid) alert("Captcha correct!");
-                else alert("Invalid captcha!");
-              }}
-            />
-
-          </Form.Item>
-               </Col>
+              <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                {loading ? (
+                  <>
+                    <SkeletonLine width="40%" />
+                    <SkeletonInput />
+                  </>
+                ) : (
+                  <Form.Item
+                    label={Language[currentLang].captcha}
+                    name="captcha"
+                  >
+                    <CaptchaBox
+                      length={6}
+                      currentLang={currentLang}
+                      onVerify={(isValid) => {
+                        if (isValid) alert("Captcha correct!");
+                        else alert("Invalid captcha!");
+                      }}
+                    />
+                  </Form.Item>
+                )}
+              </Col>
             </Row>
+            <div className="result-btn-group header-actions">
+              {loading ? (
+                <>
+                  <SkeletonButton width={120} />
+                  <SkeletonButton width={160} />
+                </>
+              ) : (
+                <>
+                  <CustomButton
+                    type="default"
+                    buttontext={Language[currentLang].cancel}
+                    classname="cancel-btn"
+                  />
 
-
-
-
-
-
-
-
-          <div className="result-btn-group header-actions">
-            <CustomButton
-              type="default"
-              buttontext="Cancel"
-              classname="cancel-btn"
-            />
-
-            <CustomButton
-              type="primary"
-              buttontext="Get Result"
-              classname="header-login-btn submit-btn"
-            />
-          </div>
-        </Form>
+                  <CustomButton
+                    type="primary"
+                    buttontext={Language[currentLang].resultButton}
+                    classname="header-login-btn submit-btn"
+                  />
+                </>
+              )}
+            </div>
+          </Form>
         </div>
-        </div>
+      </div>
     </>
   );
 };
